2095e16d8860e2e314b3aa8301b9cb2eca266f16
qwillie64 <kuan.wei.05@gmail.com>
2024-12-03 21:33:20 +0800

    Update

--- Main/MapDetail.java
+++ Main/MapDetail.java
@@ -1,8 +1,10 @@
 package Main;
 
+import Entity.ClickBlock;
+import Tool.Setting;
+import java.awt.Rectangle;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class MapDetail {
@@ -11,22 +13,49 @@ public class MapDetail {
         public int type;
         public int time;
         public int duration;
+
+        public Note(int channel, int type, int time, int duration) {
+            this.channel = channel;
+            this.type = type;
+            this.time = time;
+            this.duration = duration;
+        }
     }
     
     public int BPM;
     public int Offset;
     public int Start;
-    public ArrayList<Note> Notes;
+    public ArrayList<Note> Notes = new ArrayList<>();
 
     public void Read(String path) {
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] data = line.split(",");
+                Notes.add(new Note(
+                        Integer.parseInt(data[0]),
+                        Integer.parseInt(data[1]),
+                        Integer.parseInt(data[2]),
+                        Integer.parseInt(data[3])));
+
                 System.out.println(data[0] + data[1] + data[2]);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        for (Note note : Notes) {
+            switch (note.type) {
+                case 0:
+                    ObjectManager.AddBlock(new ClickBlock(new Rectangle(note.channel * 60, 0, 50, note.duration * 30),
+                            200, Setting.KeySet.get(note.channel)));
+                    break;
+                case 1:
+                    
+                    break;
+                default:
+                    throw new AssertionError();
+            }
         }
     }
 }
---
+++ Tool/Setting.java
@@ -0,0 +1,14 @@
+package Tool;
+
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Setting {
+    public static Map<Integer, Integer> KeySet = new HashMap<Integer, Integer>(){{
+            put(0, KeyEvent.VK_A);
+            put(1, KeyEvent.VK_S);
+            put(2, KeyEvent.VK_D);
+            put(3, KeyEvent.VK_F);
+    }};
+}
